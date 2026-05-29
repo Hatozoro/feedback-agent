8f142dfc415a93c131b429877bc07bc6dd0c7d1a
Hatozoro <sivaslihabib0@gmail.com>
2026-05-29 15:58:02 +0200

    Titel Angepasst

--- main.py
+++ main.py
@@ -312,7 +312,7 @@ def run_analysis_and_generate_html(full_history):
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>back Pro</title>
+        <title>App Feedback Dashboard</title>
         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
         <style>
